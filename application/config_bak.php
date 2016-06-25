cf1e716d3f2c368b89fca8ebff524063e1d38d1f
寒川 <misscx@msn.cn>
2016-06-25 20:37:16 +0800

    修改配置

--- application/config_bak.php
+++ application/config_bak.php
@@ -11,9 +11,15 @@
 // $Id$
 
 return [
-    'url_route_on' => true,
+    /*'url_route_on' => true,
     'log'          => [
         'type' => 'trace', // 支持 socket trace file
     ],
+	*/
 	'url'=>'http://cyadmin.com',//网站URL
+	//备份配置，一般情况下默认即可
+	'DB_PATH'     => 'db',     //数据库备份目录，框架应用根目录开始。
+	'DB_PART'     => '20971520',  //该值用于限制压缩后的分卷最大长度。单位：B；建议设置20M
+	'DB_COMPRESS' => '1',         //压缩备份文件需要PHP环境支持gzopen,gzwrite函数  0:不压缩 1:启用压缩
+	'DB_LEVEL'    => '9',         //压缩级别   1:普通   4:一般   9:最高
 ];
