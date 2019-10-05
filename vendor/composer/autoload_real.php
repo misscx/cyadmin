158fa2f81bca3209f18fa30b26ecfa8e3c915eaf
寒川 <misscx@msn.cn>
2019-10-05 10:25:50 +0800

    升级ThinkPHP至5.1.38 LTS

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit957e16ec1846e9619e5fc0f5ef39ef23
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit957e16ec1846e9619e5fc0f5ef39ef23', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
