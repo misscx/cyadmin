ea5c20ac96e60ee3e0c4b42e54dfab9559eeda3c
寒川 <misscx@msn.cn>
2019-12-24 14:10:38 +0800

    修正完善数据备份及数据恢复

--- extend/org/Data.php
+++ extend/org/Data.php
@@ -155,8 +155,16 @@ class Data {
 
             //备份数据记录
             $result = Db::query("SELECT * FROM `{$table}` LIMIT {$start}, 1000");
-            foreach ($result as $row) {
-                $sql = "INSERT INTO `{$table}` VALUES ('" . implode("', '", $row) . "');\n";
+            if(!empty($result)){
+                $sql="INSERT INTO `{$table}`  VALUES";
+                foreach ($result as $row) {
+                    $sql .= "\n("; 
+                        foreach($row as $v){
+                            $sql .=is_null($v)?'NULL,':"'".$v."',";
+                        }
+                    $sql = trim($sql,','). '),'; 
+                }
+                $sql = trim($sql,',').";\n";
                 if(false === $this->write($sql)){
                     return false;
                 }
